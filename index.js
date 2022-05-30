e439c77059f4f3fbef27ea174a0f6a8296fc76f2
htlrquotes <106533077+htlrquotes@users.noreply.github.com>
2022-05-30 14:07:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,513 @@
+const quotes = [
+
+    {
+        type: "standard",
+        quote: "Achso, wir sind sogar verheiratet, hab ich vergessen.",
+        name: "Urach"
+    },
+    {
+        type: "standard",
+        quote: "Noch schlimmer sind Donuts. Das sind Krapfen wo man das Wichtigste rausgestochen hat.",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Breunig ist kein Mangeltier, der ist halt ein echtes Viech.",
+        name: "Fink"
+    },
+    {
+        type: "standard",
+        quote: "Wenn ich mit dem Kaktus kuscheln will, dann werde ich eine herbe Enttäuschung erleben, denn er tut mit nur weh.",
+        name: "Fink"
+    },
+    {
+        type: "standard",
+        quote: "Das ist nicht Gewalt gegen Schüler, das ist Gewalt mit Schülern.",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "vvvvvvvvvvvvvrrrrrrrrrrrrrrrrrrrrrrmmmmmmmmmmmmmmmmmmmmmmm",
+        name: "Zainzis Laptop"
+    },
+    {
+        type: "standard",
+        quote: "Jerry war die Katze.",
+        name: "Withalm"
+    },
+    {
+        type: "standard",
+        quote: "Geschlechter sind wie booleans. Es gibt nur zwei.",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Lange Rede, kurzer Sinn, zünden wir was an.",
+        name: "Urach"
+    },
+    {
+        type: "standard",
+        quote: "Falls irgendjemanden etwas passiert, haben wir noch genug Schwefelsäure hinten stehen.",
+        name: "Urach"
+    },
+    {
+        type: "standard",
+        quote: "Man wird von einer Katusche Lachgas nicht High, das hab ich schon ausprobiert.",
+        name: "Urach"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Schüler", text: "„Das Tuch brennt“" },
+            { name: "Urach", text: "„Ich weiß, egal“" }
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Wer sich zu Veganismus bekennt, gibt seine Intelligenz ab",
+        name: "Fink"
+    },
+    {
+        type: "standard",
+        quote: "Wenn sogar der IQ des schlausten Schülers unter dem eines Hydranten liegt",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Dann können wir auch das n vergewaltigen.",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Da haben wieder viele gekotzt ... Das machen wir auch noch zwei mal dieses Jahr.",
+        name: "Horvat"
+    },
+    {
+        type: "standard",
+        quote: "Im Zug geht auch nicht Boom Zack in die Goschn",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Wo die ganzen N**** im Keller sitzen... achsoo, wir müssen ja politisch korrekt bleiben, N**** und N****innen",
+        name: "Schöndorfer"
+    },
+    {
+        type: "standard",
+        quote: "Wo ich meine eigene Klasse vergewaltigt hab damits gschwinder geht",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Wappler ist ein Stream",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "extrahiere eine Marmelade",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "zzzzzzrrrrrrrrrrrrrrrp",
+        name: "Dazingers Laptop"
+    },
+    {
+        type: "standard",
+        quote: "Das sagt man Tschüss mit ü",
+        name: "Fink"
+    },
+    {
+        type: "standard",
+        quote: "Das ist wie bei der Lottoziehung",
+        name: "Withalm, bei der Notenvergabe"
+    },
+    {
+        type: "standard",
+        quote: "Conk!",
+        name: "Tupay"
+    },
+    {
+        type: "standard",
+        quote: "10 plus 10 ist nicht 20!",
+        name: "Felix Ha, Schulsprecher der HTL Rennweg"
+    },
+    {
+        type: "standard",
+        quote: "Moritz, du bist so a Wappler",
+        name: "Bichl"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Nussbaumer", text: "„Inwiefern ist die Hälfte der Klasse krank?“" },
+            { name: "Schüler", text: "„geistig“" },
+            { name: "Nussbaumer", text: "„ja, nix neues“" }
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Ich hab null Freunde",
+        name: "Felix Ha, Schulsprecher der HTL Rennweg"
+    },
+    {
+        type: "standard",
+        quote: "Ich komm in meinen Rechner ned rein, er wurde neu aufgesetzt und ich hab meinen Pin vergessen",
+        name: "Fink"
+    },
+    {
+        type: "standard",
+        quote: "Und dieses Deutschland kann ich wieder unterteilen!",
+        name: "Zainzinger"
+    },
+    {
+        type: "standard",
+        quote: "Denkts einfach an LSD, das solltet ihr normalerweise nicht machen",
+        name: "Zainzinger"
+    },
+    {
+        type: "standard",
+        quote: "Achsooo, wir haben Reli! Warum bin ich überhaupt hier?",
+        name: "Felix Ha, Schulsprecher der HTL Rennweg"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Urach", text: "„Ich borg mir das mal zu Demonstrationszwecken aus“" },
+            { name: "Urach", text: "*nimmt Deo und Feuerzeug*" }
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Wozu braucht man Chloroform? Zum betäuben von Menschen, oder wenn man ausgeht, von der Frau...",
+        name: "Urach"
+    },
+    {
+        type: "standard",
+        quote: "Felix, von wem in der Klasse würdest du am ehesten sagen er ist ein Radikaler?",
+        name: "Urach"
+    },
+    {
+        type: "standard",
+        quote: "Weist was, geh einfach nicht aufs Klo, bruntz auf den Boden",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Die blauen Schüler ... Das soll jetzt kein politisches Statement sein, ... schieben wir in die 3BI rein",
+        name: "Zainzinger"
+    },
+    {
+        type: "standard",
+        quote: "Es gibt auch Leute, die können so perverse Schriften wie Chinesisch lesen. Pfffffff",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Da werd ich immer eifersüchtig, wenn er mit der Urach tanzt",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Wer misst, misst Mist",
+        name: "Völkerer"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Schüler", text: "„Das kann gar nicht sein Herr Professor, wir sind doch so klug“" },
+            { name: "Tupay", text: "„Das hab ich auch gedacht“" }
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Englisch is the yellow from the egg",
+        name: "Samek"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Eyüp", text: "„Haben Sie uns auch vermisst wie wir in London waren?“" },
+            { name: "Samek", text: "„Naja, ich hatte Freistunden“" }
+        ]
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Eyüp", text: "„Sie sind ein Viech, Herr Professor“" },
+            { name: "Wagner", text: "„Nein, ich bin nur kinderlos“" }
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Bei einer ehemaligen Schülerin die mich Daddy nennt",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Feminismus, lgbtalphabet und so weiter",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Ich bin absolut nicht woke genug",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Wem schadets wennn ich mich jetzt als Postkutsche identifizere?",
+        name: "Samek"
+    },
+    {
+        type: "standard",
+        quote: "Ein Nicht-Ziel ist es in die Waschmuschel zu pinkeln",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Ich hätte mich selbst auch nicht gewählt",
+        name: "Felix Ha, Schulsprecher der HTL Rennweg"
+    },
+    {
+        type: "standard",
+        quote: "Das währe jetzt wieder so eine klassische Tretmine in Woke-istan gewesen!",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Da braucht man wirklich künstliche Intelligenz, denn natürliche gibt es nicht mehr",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Ich hasse erfolgreiche Menschen, deshalb bin ich Lehrer",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Schulstunden sind sehr seltsame Stunden",
+        name: "Bayandor"
+    },
+    {
+        type: "standard",
+        quote: "Du erzählst uns gleich en gschmeidiges Gschichtl über die Polarisation",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Aber warum die jüngste Tochter eine Waffe hat ... wahrscheinlich falls Oracle kommt",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Was du hier machst ist bestenfalls grausam",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "Herr Professor, bevor Sie meine SMÜ kontrollieren würde ich Ihnen empfehlen 200 Gramm Balsiam zu schlucken, sonst kriegen Sie Kopfweh",
+        name: "Dylan, zu Samek"
+    },
+    {
+        type: "standard",
+        quote: "Elon Musk wird immer mehr zum James Bond bösewicht",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Du kannst auch C# verwenden, du musst halt damit rechnen, dass ich dir auf den Tisch kotze",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "So mei' Frau schreibt mir... und weg ist sie.",
+        name: "Breunig"
+    },
+    {
+        type: "standard",
+        quote: "In Pakistan gibts ja kein Internet. Wir gehen hier nur nach Äußerlichkeiten, das muss dir schon aufgefallen sein, in der Westlichen Welt.",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Wer von euch weiß warum hier Stroh liegt?",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "... mit anderen Worden, du bist eine Hure des ORF.",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Die Gummies reißen immer dann, wenn man sie braucht. Einige Beweise sitzen hier.",
+        name: "Völkerer"
+    },
+    {
+        type: "standard",
+        quote: "Das ist so als wurede ich diese Kreide jetzt hier aus dem Fenster werfen und damit die Fliege, welche im Baum sitzt treffen wuerde, die am Morgen auf dem Auto des Direktors gesessen ist.",
+        name: "Tupay"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Samek", text: "„Was ist der Zusammenhang zwischen Weg Zeit und Geschwindigkeit?“" },
+            { name: "Eyüp", text: "„Schnelligkeit“" }
+        ]
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Ha Felix", text: "„Wir übernehmen die Schule.“" },
+            { name: "Samek", text: "„Ihr übernehmts meine Nerven.“" }
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Keine Sorge ich kümmere mich darum, dass ihr nächstes Jahr weniger seid.",
+        name: "Samek"
+    },
+    {
+        type: "standard",
+        quote: "Aber der Papst ist nicht zuständig für PHP also Gummi drüber.",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Es kommt ja auf die Technik an... wie immer. Und damit die Überleitung zur Technik, unserem Unterrichtsfach.",
+        name: "Wagner"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Philip", text: "„Ich glaub du hast noch nie gut Italienisch gegessen?“" },
+            { name: "Gabriel", text: "„Doch Davids Mom.“" }
+        ]
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Breunig", text: "„Und nicht weil ich Frauen nicht mag...“" },
+            { name: "Gabriel", text: "„DOCH... also ich.“" }
+        ]
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Drucks", text: "„Fahren Sie Windows?“" },
+            { name: "Zeteny", text: "„Ja“" },
+            { name: "Drucks", text: "„Na selber Schuld.“" },
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Eine Lehrerin hat ihren damaligen Freund... wie soll ich sagen... er hat ihr in diesem Fall die mündliche Prüfung abgenommen.",
+        name: "Wagner"
+    },
+    {
+        type: "standard",
+        quote: "Das sind keine Vorurteile, das sind Urteile!",
+        name: "David"
+    },
+    {
+        type: "standard",
+        quote: "Wir fangen eh nicht an mit Feminismus und Gleichberechtigung.",
+        name: "Samek"
+    },
+    {
+        type: "standard",
+        quote: "Actually, ich hab so viele fucking Muttermale, ich glaub ich bin zu 5% schwarz",
+        name: "Cyprian"
+    },
+    {
+        type: "dialog",
+        parts: [
+            { name: "Wagner", text: "„Kim Possible, is des ned die schoafe Rothaarige?“" },
+            { name: "Gabriel", text: "„Ist die nicht 14?“" },
+        ]
+    },
+    {
+        type: "standard",
+        quote: "Genetiv 2. Fall, WESSEN Radiergummi liegt auf meinen fucking Tisch",
+        name: "Markus"
+    },
+    {
+        type: "standard",
+        quote: "Man kann euch nicht enterben, ich habs versucht.",
+        name: "Bayandor"
+    },
+];
+
+const quote = document.getElementById("quote_container");
+
+let lastQuotes = [];
+
+let curIndex;
+
+document.getElementById("previous").addEventListener("click", () => {
+    	
+    if (curIndex === 0) curIndex = quotes.length - 1; else curIndex--;
+    displayQuote(quotes[curIndex])
+    lastQuotes.push(newQuoteIndex);
+    if (lastQuotes.length > 10) lastQuotes.splice(0, 1);
+
+});
+
+document.getElementById("next").addEventListener("click", () => {
+
+    if (curIndex === quotes.length - 1) curIndex = 0; else curIndex++;
+    displayQuote(quotes[curIndex])
+    lastQuotes.push(newQuoteIndex);
+    if (lastQuotes.length > 10) lastQuotes.splice(0, 1);
+
+});
+
+document.getElementById("random").addEventListener("click", () => {
+    
+    const newQuoteIndex = getRandomQuoteIndex();
+    curIndex = newQuoteIndex;
+    displayQuote(quotes[newQuoteIndex])
+
+});
+
+
+const displayQuote = newQuote => {
+    if (newQuote.type === "standard") {
+
+        quote.innerHTML = `
+            <span class="fancy_quote">„</span>
+            <span class="quote_text">
+                ${newQuote.quote}
+            </span>
+            <span class="fancy_quote">“</span>
+            <p class="quote_name">
+                -${newQuote.name}
+            </p>
+        `;
+
+    } else if (newQuote.type === "dialog") {
+        let html = "";
+
+        for (part of newQuote.parts) html += 
+        `
+            <span class="dialog_name">${part.name}:</span>
+            <span class="quote_text">${part.text}</span>
+            <br />
+        `;
+
+        quote.innerHTML = html;
+
+    }
+}
+
+const getRandomQuoteIndex = () => {
+    let newQuoteIndex = 0;
+    while (lastQuotes.includes(newQuoteIndex)) {
+        newQuoteIndex = Math.floor(Math.random() * quotes.length)
+    }
+    lastQuotes.push(newQuoteIndex);
+    if (lastQuotes.length > 10) lastQuotes.splice(0, 1);
+    return newQuoteIndex
+}
+
+document.getElementById("random").dispatchEvent(new Event("click"));
